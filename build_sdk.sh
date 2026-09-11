f28eead1ff5a6abc72d386d900028cdfc69aff37
Guangtao Zhu <guangtaozhu@outlook.com>
2026-09-11 17:45:54 +1000

    trivial: symbolic link for sddf of uk-on-mk
    Signed-off-by: Guangtao Zhu <guangtaozhu@outlook.com>

--- build_sdk.sh
+++ build_sdk.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env sh
 
+git submodule update --init --recursive
+test ! -e ./dep/uk-on-mk/dep/sddf || test -L ./dep/uk-on-mk/dep/sddf || { echo "refusing to replace non-symlink ./dep/uk-on-mk/dep/sddf" >&2; exit 1; }
+ln -sfn ../../sddf ./dep/uk-on-mk/dep/sddf
+
 cd ./dep/microkit && \
 nix develop --command bash -c "python build_sdk.py --skip-tar --boards=x86_64_generic,x86_64_generic_vtx --sel4=../sel4 --configs=smp-debug"
 
