10a8511322c6cfc6dd7ac498c6e14dc3c0c46286
Guangtao Zhu <guangtaozhu@outlook.com>
2026-09-14 00:29:14 +1000

    trivial: automate catalog-core init & simplify run.sh
    Signed-off-by: Guangtao Zhu <guangtaozhu@outlook.com>

--- run.sh
+++ run.sh
@@ -1,15 +1,9 @@
 #!/usr/bin/env sh
 
 # Check if target machine is provided
-if [ -z "$1" ]; then
-    echo "Error: Target machine not provided."
-    echo "Usage: $0 <target_machine_name>"
-    exit 1
-fi
 
 rm -rf examples/simple/build && \
 nix develop --command bash -c "\
 make -j$(nproc) -C examples/simple MICROKIT_CONFIG=smp-debug MICROKIT_BOARD=x86_64_generic MICROKIT_SDK=../../dep/microkit/release/microkit-sdk-2.3.0-dev infra && \
 make -j$(nproc) -C examples/simple MICROKIT_CONFIG=smp-debug MICROKIT_BOARD=x86_64_generic MICROKIT_SDK=../../dep/microkit/release/microkit-sdk-2.3.0-dev apps && \
 make -j$(nproc) -C examples/simple MICROKIT_CONFIG=smp-debug MICROKIT_BOARD=x86_64_generic MICROKIT_SDK=../../dep/microkit/release/microkit-sdk-2.3.0-dev qemu"
-
